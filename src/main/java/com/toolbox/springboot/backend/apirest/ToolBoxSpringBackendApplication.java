37610a237cb385c316ef2898a931ed013a66ea17
snakeice45 <jonas171192@gmail.com>
2019-12-26 13:14:58 -0600

    api rest backend version 1 26-12-2019

---
+++ src/main/java/com/toolbox/springboot/backend/apirest/ToolBoxSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toolbox.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolBoxSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolBoxSpringBackendApplication.class, args);
+	}
+
+}
